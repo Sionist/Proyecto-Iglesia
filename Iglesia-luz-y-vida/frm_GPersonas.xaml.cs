74a0f610b3a23d203e9f89e33b743c64c59301fd
gustavocarrillo <gustavoinolympus@gmail.com>
2015-09-25 10:48:29 -0430

    3er Commit
    Modifcando algunas cosas

--- Iglesia-luz-y-vida/frm_GPersonas.xaml.cs
+++ Iglesia-luz-y-vida/frm_GPersonas.xaml.cs
@@ -9,44 +9,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Negocio;
-using Entidades;
 
 namespace Iglesia_luz_y_vida
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para frm_GPersonas.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class frm_GPersonas : Window
     {
-        public MainWindow()
+        public frm_GPersonas()
         {
             InitializeComponent();
-        }
-
-        private void btnGuardar_Click(object sender, RoutedEventArgs e)
-        {
-            persona persona = new persona();
-            persona.cedula = txtCedula.Text;
-            persona.nombres = txtNombres.Text;
-            persona.apellidos = txtApellidos.Text;
-            persona.direccion = txtDireccion.Text;
-            //persona.telefono = txtTelefono.Text;
-            persona.email = txtEmail.Text;
-            persona.fecha_nacimiento = Convert.ToDateTime(dtFechaNacimiento.Text);
-            //persona.estatus = txtEstatus.Text;
-
-            personaNC personaNC = new personaNC();
-            personaNC.nuevo(persona);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            personaNC personaNC = new personaNC();
-            dgCoincidencias.ItemsSource = personaNC.traerTodos();
-            
         }
     }
 }
